ba96112bfe7375112fe841c4259f3903f844246f
Nathan Hentges <nathanjhentges@gmail.com>
2021-05-14 13:40:54 -0500

    deleted extra files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const routes = require('./routes');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,21 +10,21 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 // Add routes, both API and view
 app.use(routes);
 
-app.use("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/onlineproducts');
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, () => {
+  console.log(`API Server now listening on PORT ${PORT}!`);
 });
