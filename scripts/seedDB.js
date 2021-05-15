e19267b787ff3f1fe82f9ccd2d3464bbc9bec3fe
Kelsie <kelsie51@hotmail.com>
2021-05-15 18:31:24 -0500

    Established images folder, inserted two edited images. Started seed files.

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-const db = require("../models/products");
+const db = require("../models");
 
-// This file empties the Books collection and inserts the books below
 
 mongoose.connect(
   process.env.MONGODB_URI ||
@@ -12,73 +11,29 @@ const userSeed = [
   {
     name: "Luke Paschka",
     email: "lpaschka@emaildomain.com",
-    age: "45",
-    role: "Student"
-  },
-  {
-    name: "Johnny Test",
-    email: "jtest@emaildomain.com",
-    age: "15",
-    role: "Child"
-  },
-  {
-    name: "Kristin Paschka",
-    email: "kpaschka@emaildomain.com",
-    age: "40",
-    role: "My Wife"
-  },
-  {
-    name: "Austen Staydohar",
-    email: "astaydohar@emaildomain.com",
-    age: "25",
-    role: "Son"
-  },
-  {
-    name: "Zoey Paschka",
-    email: "zpaschka@emaildomain.com",
-    age: "19",
-    role: "Daughter"
-  },
-  {
-    name: "Tera Cunninham",
-    email: "tcunningham@emaildomain.com",
-    age: "19",
-    role: "Student"
-  },
-  {
-    name: "Tegan Cunningham",
-    email: "tgcunningham@emaildomain.com",
-    age: "14",
-    role: "Student"
-  },
-  {
-    name: "This Guy",
-    email: "tguy@emaildomain.com",
-    age: "28",
-    role: "Superviser"
-  },
-  {
-    name: "Deb Borah",
-    email: "dborah@emaildomain.com",
-    age: "38",
-    role: "Student"
-  },
-  {
-    name: "Luke Paschka",
-    email: "lpaschka@emaildomain.com",
-    age: "45",
-    role: "My Twin"
-  },
+    password: "45",
+    phone: "763-555-5555",
+    address: "1234 declarathumbwar dr",
+  }
 ];
 
-// db.User
-//   .remove({})
-//   .then(() => db.User.collection.insertMany(userSeed))
-//   .then(data => {
-//     console.log(data.result.n + " records inserted!");
-//     process.exit(0);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
+db.User
+  .remove({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+
+// Users:
+// username
+// password
+// email
+// phone
+// address
+
