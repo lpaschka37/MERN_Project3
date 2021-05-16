54ed2eb0a42e9207f30525abe8855e6bb382ed88
Nathan Hentges <nathanjhentges@gmail.com>
2021-05-16 13:43:18 -0500

    basic CRUD for db controllers

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,35 +1,35 @@
 const db = require("../models");
 
-// Defining methods for the userController
+// Defining methods for the productssController
 module.exports = {
 
   findAll(req, res) {
-    db.User
+    db.Products
       .find()
       .sort({ date: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findById(req, res) {
-    db.User
+    db.Products
       .findById(req.params.id)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   create(req, res) {
-    db.User
+    db.Products
       .create(req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   update(req, res) {
-    db.User
+    db.Products
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   remove(req, res) {
-    db.User
+    db.Products
       .findById({ _id: req.params.id })
       .then((dbModel) => dbModel.remove())
       .then((dbModel) => res.json(dbModel))
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 // Connect to the Mongo DB
