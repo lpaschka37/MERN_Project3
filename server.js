3fc1022dfc42cca5153c50acceb0336aeb871ba9
Luke Paschka <lpaschka37@gmail.com>
2021-05-16 16:47:53 -0500

    adding html files

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const routes = require("./routes");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -16,11 +15,14 @@ if (process.env.NODE_ENV === "production") {
 
 // Add routes, both API and view
 app.use(routes);
+app.use("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/onlineproducts");
 
 // Start the API server
-app.listen(PORT, () => {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function() {
+  console.log(`:earth_americas:  ==> API Server now listening on PORT ${PORT}!`);
 });
