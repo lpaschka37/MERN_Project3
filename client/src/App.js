21b7f2dc6197e2f83261bbb78fd7eb3f39adbc0c
Nathan Hentges <nathanjhentges@gmail.com>
2021-05-18 16:20:21 -0500

    Added basic routing paths for all pages

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,34 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Header from "./components/Header/Header";
 import NavTop from "./components/NavTop/NavTop";
 import Footer from "./components/Footer/Footer";
-import ProductListPage from "./pages/Category4";
-import CheckOut from "./pages/Checkout";
-// import SignIn from "./pages/Signin";
 
-// import Home from "./pages/Home";
-// import ProductListPage  from "./pages/Category4";
-// import CheckOut from "./pages/Checkout";
+import Home from "./pages/Home";
+import Vinyl from "./pages/Category1";
+import Green from "./pages/Category2";
+import Games from "./pages/Category3";
+import Party from "./pages/Category4";
+import SignIn from "./pages/Signin";
+import CheckOut from "./pages/Checkout";
 
 function App() {
   return (
 
   <Router>
-     <NavTop />
-        {/* <Route exact path="/" component={HomePage} /> */}
-        <Route exact path="/productlistpage" component={ProductListPage} />
-        <Route exact path="/checkout" component={CheckOut} />
-        {/* <Route exact path="/signin" component={SignIn} /> */}
-      <Footer />
+    <NavTop />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/vinyl" component={Vinyl} />
+    <Route exact path="/green" component={Green} />
+    <Route exact path="/games" component={Games} />
+    <Route exact path="/party" component={Party} />
+    <Route exact path="/signin" component={SignIn} />
+    <Route exact path="/checkout" component={CheckOut} />
+    <Footer />
   </Router>
   );
 }
 
 export default App;
+
+/**
+ * Need to decide on which pages are used, their names, etc
+ */
