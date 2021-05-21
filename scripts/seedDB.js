347e52ab20b147e4be364aa80d312dbe49bff199
Kelsie <kelsie51@hotmail.com>
2021-05-20 22:22:54 -0500

    Re-added image. Corrected typo

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -63,7 +63,7 @@ const productsSeed = [
   },
   
   {
-    productImage: "garybook.png",
+    productImage: "gary-book.png",  
     productName: "Hello, My Name is Gary",
     description: "One hundred of everyone's favorite Garys all together for the first time.",
     price: 22.49,
